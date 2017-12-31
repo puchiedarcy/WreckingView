18cf8ebe93289cbc9f820d30b2fcf06ade8e35a8
Zach Layman <puchiedarcy@puchiedarcy.com>
2017-12-31 15:53:47 -0500

    login on every page w nav bar

--- common.go
+++ common.go
@@ -6,6 +6,6 @@ import (
 )
 
 func Render(w http.ResponseWriter, templateName string, content interface{}) {
-    t, _ := template.ParseFiles("views/layout.html", templateName)
+    t, _ := template.ParseFiles("views/layout.html", "views/login.html", templateName)
     t.ExecuteTemplate(w, "layout", content)
 }
